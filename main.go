d2580ba05389c6a7535e09fde637174c717b3f24
SimonasKalpokas <simonas.kalpokas@gmail.com>
2024-07-15 15:19:49 +0300

    Add grid

--- main.go
+++ main.go
@@ -7,15 +7,47 @@ package main
 import "C"
 import "unsafe"
 
+type World struct {
+	// logic
+	columnCount int
+	rowCount    int
+
+	// rendering
+	boxSize int
+}
+
+func (world *World) ScreenHeight() int {
+	return world.rowCount * world.boxSize
+}
+
+func (world *World) ScreenWidth() int {
+	return world.columnCount * world.boxSize
+}
+
+func (world *World) Draw() {
+	for row := 0; row < world.rowCount; row++ {
+		for column := 0; column < world.columnCount; column++ {
+			C.DrawRectangleLines(
+				C.int(column*world.boxSize),
+				C.int(row*world.boxSize),
+				C.int(world.boxSize),
+				C.int(world.boxSize),
+				C.GRAY)
+		}
+	}
+}
+
 func main() {
 	title := C.CString("Title")
 	defer C.free(unsafe.Pointer(title))
-	C.InitWindow(800, 450, title)
+
+	world := World{columnCount: 40, rowCount: 20, boxSize: 20}
+	C.InitWindow(C.int(world.ScreenWidth()), C.int(world.ScreenHeight()), title)
 
 	for !C.WindowShouldClose() {
 		C.BeginDrawing()
-		C.ClearBackground(C.RAYWHITE)
-		C.DrawText(C.CString("Congrats!"), 190, 200, 20, C.LIGHTGRAY)
+		C.ClearBackground(C.LIGHTGRAY)
+		world.Draw()
 		C.EndDrawing()
 	}
 	C.CloseWindow()
